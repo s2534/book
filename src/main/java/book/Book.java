9c0d8df641d4a5529c1eb11794075f31150e8f43
unknown <s25-30414@bundang-mgt.hs.kr>
2025-08-22 12:49:03 +0900

    four

---
+++ src/main/java/book/Book.java
@@ -0,0 +1,25 @@
+package book;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String publisher;
+    private int price;
+
+    // Getter / Setter
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getPublisher() { return publisher; }
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+}
